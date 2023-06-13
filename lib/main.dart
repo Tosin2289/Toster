e897d6a4af77b54da3636a3768aec0c512ee6fb6
Tosin2289 <ayoolatosin00@gmail.com>
2023-06-13 22:54:13 +0100

    redesign

--- lib/main.dart
+++ lib/main.dart
@@ -23,10 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        primarySwatch: Colors.deepPurple,
-      ),
+      theme: ThemeData(useMaterial3: true, primaryColor: Colors.green),
       home: Splash(),
     );
   }
